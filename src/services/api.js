2a70abb64ace84f2654477bd0103fba1833b1a88
Linh Nguyen <nvlinh17041992@gmail.com>
2024-12-06 07:08:52 +0700

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const API = {
   call() {
     return axios.create({
-      baseURL: 'http://wp-api.test/wp-json/',
+      baseURL: 'https://wp-api.codethanhthuongthua.asia/wp-json/',
     });
   },
   callWithToken: function (token) {
     if (!token) token = localStorage.getItem('ACCESS_TOKEN');
 
     return axios.create({
-      baseURL: 'http://wp-api.test/wp-json/',
+      baseURL: 'https://wp-api.codethanhthuongthua.asia/wp-json/',
       headers: {
         Authorization: `Bearer ${token}`,
       },
